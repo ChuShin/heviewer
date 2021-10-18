33be3e3b99e8c9be551108f72e227ee4595086f4
chk512 <kevin.koh@gifs.ca>
2021-10-18 09:11:33 -0600

    Updates

---
+++ precache-manifest.11217c8a0fc5c32c174c4934584f355d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d011275ae44236d833dc",
+    "url": "/heviewer/static/css/main.835ea200.chunk.css"
+  },
+  {
+    "revision": "d011275ae44236d833dc",
+    "url": "/heviewer/static/js/main.4556d3c4.chunk.js"
+  },
+  {
+    "revision": "21dfcff993fa1623b7e4",
+    "url": "/heviewer/static/js/runtime~main.53ecdcc3.js"
+  },
+  {
+    "revision": "8e671b9d7b70edf9181d",
+    "url": "/heviewer/static/js/2.a1e0d1c7.chunk.js"
+  },
+  {
+    "revision": "38b7d2192f5b07bc7522",
+    "url": "/heviewer/static/js/3.49617b77.chunk.js"
+  },
+  {
+    "revision": "2afc79efd03ba7306879b9693975bb9c",
+    "url": "/heviewer/index.html"
+  }
+];
