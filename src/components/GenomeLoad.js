aae2a63f317e79baa24b68f55a506a0386eaad1b
ChuShin <chushin.koh@gmail.com>
2022-04-03 21:23:07 -0700

    custom file loader.

--- src/components/GenomeLoad.js
+++ src/components/GenomeLoad.js
@@ -4,7 +4,7 @@ import { csv, tsv } from 'd3-fetch'
 import { ascending } from 'd3-array'
 
 import GenomeGroup from './GenomeGroup'
-import FileLoader from './FileLoader'
+//import FileLoader from './FileLoader'
 
 function heFormat(d) {
   return {
@@ -20,20 +20,39 @@ const GenomeLoad = () => {
   const [genome, setGenomeData] = useState(null)
 
   useEffect(() => {
-    csv('./data/bna_he.csv',heFormat).then(data => {
-      setGenomeData(data)
-    })
+    // csv('./data/bna_he.csv',heFormat).then(data => {
+    //  setGenomeData(data)
+    // })
+    function handleFileSelect(evt) {
+      var file = evt.target.files[0];
+      var reader = new FileReader();
+      reader.onload = (function(theFile) {
+      return function(e) {
+        //drawChart(e.target.result);
+        console.log(e.target.result)
+        csv(e.target.result,heFormat).then(data => {
+          setGenomeData(data)
+        })
+      };
+    })(file);
+    reader.readAsDataURL(file);
+}
+document.getElementById('file_input').addEventListener('change', handleFileSelect, false);
+
+
   }, [])
 
 
-  if (genome === null) {
+  //if (genome === null) {
     // add a loader timeout in 10s
-    return <h3>Loading data..</h3>
-  }
+  //  return <h3>Loading data..</h3>
+  //}
 
   return (
   <div>
-  <FileLoader />
+  <input type="file" id="file_input" accept=".csv"
+  />
+
   <GenomeGroup data={genome} />
   </div>
   )
